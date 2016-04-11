9a4cb31e9cc5969b0b0fbd84ca4f84d107ad6b78
Kevin Qiu <kevinqiu1@gmail.com>
2016-04-10 22:57:49 -0400

    update to support last quotes, logging, other stuff

--- quotes.py
+++ quotes.py
@@ -1,7 +1,10 @@
+import re
 import json
 import requests
 import twilio
 import flask
+import tinydb
+import logging
 from tinydb import TinyDB, Query
 from flask import Flask, request
 from twilio import twiml
@@ -13,45 +16,49 @@ with open('config.json', 'r') as config_file:
 
 quandl_key = config.get('quandl_key')
 message_templates = config.get('message_templates')
+db = tinydb.TinyDB('db.json')
 
-def save_symbol(number, sym):
+def save_symbols(number, syms):
+    NumberEntry = Query()
+    number_symbol_entry = { 'number': number, 'symbols': syms }
+    updated = db.update(number_symbol_entry, NumberEntry.number == number)
+    if len(updated) == 0:
+        db.insert(number_symbol_entry)
+    logging.info('Symbols saved for ', number)
     return
 
-def quandl_url(db, sym):
-    return 'https://www.quandl.com/api/v3/datasets/'+ db+ '/' + sym + '/data.json'
-
-def parse_stock_info(result_json):
-    response_data = result_json.get('dataset_data')
-    headers = response_data.get('column_names')
-    data_row = response_data.get('data')[0]
-    return dict(zip(headers, data_row))
-
-def get_stock_quote_quandl(db, sym):
-    params = {'api_key':quandl_key, 'rows': 1}
-    url = quandl_url(db, sym)
-    result = requests.get(url, params=params).json()
-    quoteDict = parse_stock_info(result)
-    return {'symbol': sym,
-            'price': quoteDict.get('Close')}
+def retrieve_symbols(number):
+    logging.info('Retrieving symbols for ', number)
+    NumberEntry = Query()
+    results = db.search(NumberEntry.number == number)
+    return results[0]['symbols'] if len(results) > 0 else []
 
 def get_stock_quote_markit(sym):
-    markit = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/json'
+    markitUrl = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/json'
     params = {'symbol': sym }
-    result = requests.get(url, params=params).json()
-    return { 'symbol': result.get('Symbol'),
+    result = requests.get(markitUrl, params=params).json()
+    price = result.get('LastPrice')
+    return { 'symbol': sym,
              'price': result.get('LastPrice') }
 
 def format_quote(quote):
-    quote_str = Template(message_templates.get('quote_template'))
-    quote_str.substitute(symbol = quote['symbol'], price = quote['price'])
-    return quote_str
+    quote_tmp = Template(message_templates.get('quote_template'))
+    if quote['price'] != None:
+        price = quote['price']
+    else:
+        price = 'Invalid Symbol'
+    return quote_tmp.substitute(symbol = quote['symbol'], price = price)
 
 def format_quotes(quotesList):
-    quote_strings = map(quotesList, format_quote)
-    return quote_strings.join(',')
+    quote_strings = list(map(format_quote, quotesList))
+    return ', '.join(quote_strings)
+
+def validQuote(quote):
+    return quote['price'] != None
 
 def get_quotes(symbolList):
-    quotes =  map(symbolList, get_stock_quote_markit)
+    logging.info('Retrieving quotes for ', symbolList)
+    quotes =  list(map(get_stock_quote_markit, symbolList))
     return format_quotes(quotes)
 
 def twiml_response(message, phone_number):
@@ -65,10 +72,13 @@ def get_help_twiml(phone_number):
     help_message = message_templates.get('help_message')
     return twiml_response(help_message, phone_number)
 
-def get_quote_twiml(symbol, phone_number):
-    quote = get_quotes([symbol])
-    print(quote)
+def get_quotes_twiml(symbols, phone_number):
+    quote = get_quotes(symbols)
     return twiml_response(quote, phone_number)
+
+def extract_symbols(text):
+    symbols = re.findall(r'\$(\w*)', text)
+    return list(map(str.upper, symbols))
 
 app = Flask(__name__)
 
@@ -76,9 +86,23 @@ app = Flask(__name__)
 def recieve_text():
     number = request.form['From']
     text = request.form['Body']
-    print(request)
-    if text[0] == '$':
-        response_twiml = get_quote_twiml(text[1:], number)
+
+    if bool(re.match('last', text, re.I)):
+        symbols = retrieve_symbols(number)
+    else:
+        symbols = extract_symbols(text)
+
+    if len(symbols) > 0:
+        try:
+            save_symbols(number, symbols)
+        except:
+            logging.error('Could not save symbols for number', number)
+            #log save error
+        try:
+            response_twiml = get_quotes_twiml(symbols, number)
+        except:
+            error = message_templates.get('error_message')
+            response_twiml = twiml_response(error, number)
     else:
         response_twiml = get_help_twiml(number)
     return response_twiml
